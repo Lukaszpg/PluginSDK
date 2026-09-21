d181f02762cf70bd1b9d1c8741ec43b1870f99c9
Łukasz Górny <lukasz.gorny@netguru.com>
2026-09-21 23:39:45 +0200

    Update ItemService ABI layout assertions for additive affix extension

--- tests/service_contract_tests.cpp
+++ tests/service_contract_tests.cpp
@@ -101,7 +101,7 @@ static_assert(offsetof(D2RL::InventoryService, getEquippedItem) == 96);
 static_assert(offsetof(D2RL::InventoryService, forEachInventoryItem) == 104);
 static_assert(D2RL::InventoryServiceRequiredSize == 112);
 
-static_assert(sizeof(D2RL::ItemService) == 64);
+static_assert(sizeof(D2RL::ItemService) == 72);
 static_assert(alignof(D2RL::ItemService) == 8);
 static_assert(static_cast<uint32_t>(D2RL::ItemService::Id) == 15);
 static_assert(D2RL::ItemService::AbiVersion == 1);
@@ -114,6 +114,8 @@ static_assert(offsetof(D2RL::ItemService, destroyItem) == 32);
 static_assert(offsetof(D2RL::ItemService, executeTransaction) == 40);
 static_assert(offsetof(D2RL::ItemService, editNativeItem) == 48);
 static_assert(offsetof(D2RL::ItemService, executeExistingItemTransaction) == 56);
+static_assert(offsetof(D2RL::ItemService, augmentItemAffix) == 64);
+static_assert(D2RL::ItemServiceAffixAugmentRequiredSize == 72);
 static_assert(D2RL::ItemServiceRequiredSize == 64);
 
 static_assert(sizeof(D2RL::ItemInteractionService) == 24);
